f6f6da7354b890c0cf29afad44edd04f66221dc4
Brightori <35464150+Brightori@users.noreply.github.com>
2020-02-24 01:56:12 +0300

    update, add injections and register pattern

--- EventAggregator/EventAggregator.cs
+++ EventAggregator/EventAggregator.cs
@@ -10,27 +10,27 @@ namespace GlobalEventAggregator
     public class EventContainer<T> : IDebugable
     {
         private event EventHandler<T> _eventKeeper;
-        private readonly Dictionary<WeakReference, EventHandler<T>> _activeListenersOfThisType = new Dictionary<WeakReference, EventHandler<T>>();
+        private readonly HashSet<OwnWeakReference<T>> _activeListenersOfThisType = new HashSet<OwnWeakReference<T>>();
         private const string Error = "null";
 
         public bool HasDuplicates(object listener)
         {
-            return _activeListenersOfThisType.Keys.Any(k => k.Target == listener);
+            return _activeListenersOfThisType.Any(k => k.holder == listener);
         }
 
         public void AddToEvent(object listener, EventHandler<T> action)
         {
-            var newAction = new WeakReference(listener);
-            _activeListenersOfThisType.Add(newAction, action);
-            _eventKeeper += _activeListenersOfThisType[newAction];
+            var newAction = new OwnWeakReference<T>(listener, action);
+            _activeListenersOfThisType.Add(newAction);
+            _eventKeeper += newAction.EventHandler;
         }
 
         public void RemoveFromEvent(object listener)
         {
-            var currentEvent = _activeListenersOfThisType.Keys.FirstOrDefault(k => k.Target == listener);
-            if (currentEvent != null)
+            var currentEvent = _activeListenersOfThisType.FirstOrDefault(k => k.holder.Target == listener);
+            if (currentEvent.holder != null)
             {
-                _eventKeeper -= _activeListenersOfThisType[currentEvent];
+                _eventKeeper -= currentEvent.EventHandler;
                 _activeListenersOfThisType.Remove(currentEvent);
             }
         }
@@ -38,17 +38,17 @@ namespace GlobalEventAggregator
         public EventContainer(object listener, EventHandler<T> action)
         {
             _eventKeeper += action;
-            _activeListenersOfThisType.Add(new WeakReference(listener), action);
+            _activeListenersOfThisType.Add(new OwnWeakReference<T>(listener, action));
         }
 
         public void Invoke(T t)
         {
-            if (_activeListenersOfThisType.Keys.Any(k => k.Target.ToString() == Error))
+            if (_activeListenersOfThisType.Any(k=> k.IsDead()))
             {
-                var failObjList = _activeListenersOfThisType.Keys.Where(k => k.Target.ToString() == Error).ToList();
-                foreach (var fail in failObjList)
+                var failObjList = _activeListenersOfThisType.Where(k => k.IsDead());
+                foreach (var fail in failObjList.ToArray())
                 {
-                    _eventKeeper -= _activeListenersOfThisType[fail];
+                    _eventKeeper -= fail.EventHandler;
                     _activeListenersOfThisType.Remove(fail);
                 }
             }
@@ -61,9 +61,11 @@ namespace GlobalEventAggregator
         public string DebugInfo()
         {
             string info = string.Empty;
-            foreach (var c in _activeListenersOfThisType.Keys)
+            info += _activeListenersOfThisType.Count.ToString() + "Count \n";
+            foreach (var c in _activeListenersOfThisType)
             {
-                info += c.Target.ToString() + "\n";
+                if (c.holder.Target != null)
+                    info += c.holder.Target.ToString() + "\n";
             }
             return info;
         }
@@ -72,6 +74,8 @@ namespace GlobalEventAggregator
     public static class EventAggregator
     {
         private static Dictionary<Type, object> GlobalListeners = new Dictionary<Type, object>();
+        private static Dictionary<Type, List<ActionContainer>> DelayedInjection = new Dictionary<Type, List<ActionContainer>>();
+        private static Dictionary<Type, dynamic> SingleInjections = new Dictionary<Type, dynamic>();
 
         static EventAggregator()
         {
@@ -80,7 +84,14 @@ namespace GlobalEventAggregator
 
         private static void ClearGlobalListeners(Scene scene)
         {
-            GlobalListeners.Clear();
+            ForceClean();
+        }
+
+        public static void ForceClean()
+        {
+            GlobalListeners = new Dictionary<Type, object>();
+            DelayedInjection = new Dictionary<Type, List<ActionContainer>>();
+            SingleInjections = new Dictionary<Type, dynamic>();
         }
 
         public static void AddListener<T>(object listener, Action<T> action)
@@ -108,6 +119,45 @@ namespace GlobalEventAggregator
             eventContainer.Invoke(data);
         }
 
+        public static void RegisterInject<T>(T obj)
+        {
+            var key = typeof(T);
+            if (DelayedInjection.ContainsKey(key)) 
+            {
+                foreach (var delayed in DelayedInjection[key])
+                    delayed.InjectFunc(obj);
+
+                DelayedInjection[key].Clear();
+                DelayedInjection.Remove(key);
+            }
+
+            SingleInjections.Add(key, obj);
+        }
+        
+        public static void Inject<T>(Action<T> obj)
+        {
+            var key = typeof(T);
+
+            if (SingleInjections.ContainsKey(key))
+            {
+                if (SingleInjections[key] == null)
+                {
+                    SingleInjections.Remove(key);
+                    Inject(obj);
+                    return;
+                }
+                    
+                obj.Invoke(SingleInjections[key]);
+            }
+            else
+            {
+                if (DelayedInjection.ContainsKey(key))
+                     DelayedInjection[key].Add(new InjectAction<T>(obj));
+                else
+                    DelayedInjection.Add(key, new List<ActionContainer> { new InjectAction<T>(obj)});
+            }
+        }
+
         public static void RemoveListener<T>(object listener)
         {
             var key = typeof(T);
@@ -116,6 +166,37 @@ namespace GlobalEventAggregator
                 var eventContainer = (EventContainer<T>)GlobalListeners[key];
                 eventContainer.RemoveFromEvent(listener);
             }
+        }
+
+        public static void RegisterObjectByEvent<T>(T registerObject, bool stateType)
+        {
+            Invoke(new Register<T> { RegisterObject = registerObject, Add = stateType });
+        }
+
+        public static void AddOrRemoveElement<T>(this List<T> list, T element, bool add)
+        {
+            if (add)
+            {
+                if (list.Contains(element))
+                    return;
+                list.Add(element);
+            }
+            else
+                if (list.Contains(element))
+                list.Remove(element);
+        }
+
+        public static void RegisterEventObjectAtList<T>(this List<T> registrationList, Register<T> registerEvent)
+        {
+            if (registerEvent.Add)
+            {
+                if (registrationList.Contains(registerEvent.RegisterObject))
+                    return;
+                registrationList.Add(registerEvent.RegisterObject);
+            }
+            else
+                if (registrationList.Contains(registerEvent.RegisterObject))
+                    registrationList.Remove(registerEvent.RegisterObject);
         }
 
         public static string DebugInfo()
@@ -133,8 +214,58 @@ namespace GlobalEventAggregator
         }
     }
 
+    public struct OwnWeakReference<T>
+    {
+        public WeakReference holder;
+        public EventHandler<T> EventHandler;
+
+        public OwnWeakReference(object obj, EventHandler<T> eventHandler)
+        {
+            holder = new WeakReference(obj);
+            EventHandler = eventHandler;
+        }
+
+        public bool IsDead()
+        {
+            return holder.Target == null || holder.Target.ToString() == "null" || !holder.IsAlive;
+        }
+    }
+
+    public struct Register<T>
+    {
+        public T RegisterObject;
+        public bool Add;
+
+        public Register(T registerObject, bool add)
+        {
+            RegisterObject = registerObject;
+            Add = add;
+        }
+    }
+
     public interface IDebugable
     {
         string DebugInfo();
     }
+
+    public abstract class ActionContainer
+    {
+        public abstract void InjectFunc(dynamic call);
+    }
+
+    public class InjectAction<T> : ActionContainer
+    {
+        public InjectAction(Action<T> inject)
+        {
+            Inject = inject;
+        }
+
+        public Action<T> Inject { get; private set; }
+        public override void InjectFunc(dynamic call)
+        {
+            Inject?.Invoke(call);
+        }
+    }
+
 }
+
